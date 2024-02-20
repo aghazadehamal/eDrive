bdb5eec5dc9e1e49892e7bd4397c4359e043bb9d
aghazadehamal@gmail.com <aghazadehamal@gmail.com>
2024-02-20 14:57:51 +0400

    new

--- src/LandingPage/Navbar.js
+++ src/LandingPage/Navbar.js
@@ -6,14 +6,14 @@ function Navbar() {
   return (
     <div className="customNavbar">
       <img  src="/edurive.jpg" alt="Novademy Logo" style={{ maxWidth: '250px'}} />
-      <nav className="customNav">
+      <nav  className="customNav">
         <a href="#about" className="customNavLink">Niyə biz?</a> {/* Linkler */}
         <a href="#mentor" className="customNavLink">Mentorumuz haqqında</a>
         <a href="#courses" className="customNavLink">Kursumuz haqqında</a>
       </nav>
       <div className="customButtons">
        
-        <Link  to="/LoginForm"><button className="customBtn customLogin">Daxil ol</button> </Link>
+        <Link  to="/LoginForm"><button className="customBtn customLogin"><p>Daxil ol</p></button> </Link>
        
        <Link to="/RegistrationForm">     <button className="customBtn customRegister">Qeydiyyatdan keç</button> </Link>
       </div>

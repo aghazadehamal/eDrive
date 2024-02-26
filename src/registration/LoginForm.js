e344f064033c00cb4b4aa3b6b22afd57d80044d7
aghazadehamal@gmail.com <aghazadehamal@gmail.com>
2024-02-26 23:36:42 +0400

    new

--- src/registration/LoginForm.js
+++ src/registration/LoginForm.js
@@ -1,13 +1,13 @@
-import React, { useState } from 'react';
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import './LoginForm.css'; // İlgili CSS stil dosyası
+import React, { useState } from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import "./LoginForm.css"; 
 
 function LoginForm() {
   const [formData, setFormData] = useState({
-    gmail: '',
-    password: '',
+    gmail: "",
+    password: "",
   });
 
   const navigate = useNavigate();
@@ -19,36 +19,40 @@ function LoginForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      const response = await axios.post('https://edurive.onrender.com/auth/login', formData);
+      const response = await axios.post(
+        "https://edurive.onrender.com/auth/login",
+        formData
+      );
       if (response.data.accessToken) {
-        localStorage.setItem('userToken', response.data.accessToken);
-        navigate('/lessonss?showModal=true'); // Burada URL parametresini ekleyin
+        localStorage.setItem("userToken", response.data.accessToken);
+        navigate("/lessonss?showModal=true");
       } else {
-        console.error('Giriş başarısız, token alınamadı.');
+        console.error("Giriş başarısız, token alınamadı.");
       }
     } catch (error) {
       if (error.response && error.response.data) {
-        console.error('Giriş yapılırken bir hata oluştu:', error.response.data);
+        console.error("Giriş yapılırken bir hata oluştu:", error.response.data);
       } else if (error.request) {
-        console.error('Yanıt alınamadı:', error.request);
+        console.error("Yanıt alınamadı:", error.request);
       } else {
-        console.error('İstek sırasında bir hata oluştu:', error.message);
+        console.error("İstek sırasında bir hata oluştu:", error.message);
       }
     }
   };
 
   return (
-    <div style={{textAlign: "center"}} className="login">
+    <div style={{ textAlign: "center" }} className="login">
       <Link to="/">
-        <img src="/edurive.svg" alt="Edurive Logo" style={{ maxWidth: '250px', marginTop: "20px" }} />
+        <img
+          src="/edurive.svg"
+          alt="Edurive Logo"
+          style={{ maxWidth: "250px", marginTop: "20px" }}
+        />
       </Link>
       <form onSubmit={handleSubmit}>
-        <h1 style={{color: "black"}}>Daxil ol</h1>
-        <p>
-          E-mail
-        </p>
+        <h1 style={{ color: "black" }}>Daxil ol</h1>
+        <p>E-mail</p>
         <input
-        
           type="email"
           name="gmail"
           value={formData.gmail}
@@ -56,9 +60,7 @@ function LoginForm() {
           placeholder="E-mailinizi əlavə edin"
           required
         />
-         <p>
-          Şifrə
-        </p>
+        <p>Şifrə</p>
         <input
           type="password"
           name="password"
@@ -67,15 +69,30 @@ function LoginForm() {
           placeholder="Şifrənizi əlavə edin"
           required
         />
-        <span style={{color: "#50BB27", marginLeft: "250px"}}>Şifrəmi unutdum</span>
+        <span style={{ color: "#50BB27", marginLeft: "250px" }}>
+          Şifrəmi unutdum
+        </span>
         <button type="submit">Daxil ol</button>
 
-
-
-        <div style={{  marginLeft: "50px", marginTop: "42px", height: "40px", width: "384px" }}>
-          <span style={{color: "#1F203F" }}>Hesabınız yoxdur?</span>
-          {/* Burada 'Daxil ol' metni için Link bileşenini kullanıyoruz */}
-          <Link to="/RegistrationForm" style={{ marginLeft: '10px', color: "green", textDecoration: "none" }}>Qeydiyyatdan keçin</Link>
+        <div
+          style={{
+            marginLeft: "50px",
+            marginTop: "42px",
+            height: "40px",
+            width: "384px",
+          }}
+        >
+          <span style={{ color: "#1F203F" }}>Hesabınız yoxdur?</span>
+          <Link
+            to="/RegistrationForm"
+            style={{
+              marginLeft: "10px",
+              color: "green",
+              textDecoration: "none",
+            }}
+          >
+            Qeydiyyatdan keçin
+          </Link>
         </div>
       </form>
     </div>
